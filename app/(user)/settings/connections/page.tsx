5a47bb5c93652bce30ca0d9115382abd1037cdd5
Julian <chetkara@gmail.com>
2026-07-31 02:17:09 +0300

    renaming file

--- app/(user)/settings/connections/page.tsx
+++ app/(user)/settings/connections/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useTransition } from "react";
-import SettingsBase from "../settingsBase2";
+import SettingsBase from "../settingsBase";
 import { Input } from "@barrelrolla/react-components-library";
 
 export default function ConnectionsSettingsPage() {
