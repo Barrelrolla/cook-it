838e44e989d175ba809cdcb870bfa8533f3f90a5
Julian <chetkara@gmail.com>
2026-07-31 17:49:32 +0300

    added confirmation dialogs

--- app/(user)/settings/connections/unlinkSocialButton.tsx
+++ app/(user)/settings/connections/unlinkSocialButton.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { authClient } from "@/auth/authClient";
-import { Button } from "@barrelrolla/react-components-library";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardText,
+  CardTitle,
+  Dialog,
+} from "@barrelrolla/react-components-library";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -10,11 +17,13 @@ export default function UnlinkSocialButton({
 }: {
   social: "google" | "apple";
 }) {
+  const [isOpen, setIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const router = useRouter();
 
   function unlink() {
+    setIsOpen(false);
     authClient.unlinkAccount(
       { providerId: social },
       {
@@ -34,7 +43,35 @@ export default function UnlinkSocialButton({
   }
   return (
     <>
-      <Button loading={isLoading} size="xs" color="error" onClick={unlink}>
+      <Dialog isOpen={isOpen} setIsOpen={setIsOpen}>
+        <Card className="w-[90vw] max-w-60">
+          <form action={unlink}>
+            <CardTitle className="font-heading">Unlink account</CardTitle>
+            <CardText>Are you sure?</CardText>
+            <CardActions className="flex justify-between w-full">
+              <Button
+                color="success"
+                size="sm"
+                type="button"
+                onClick={() => setIsOpen(false)}
+              >
+                NO
+              </Button>
+              <Button size="sm" color="error">
+                YES
+              </Button>
+            </CardActions>
+          </form>
+        </Card>
+      </Dialog>
+      <Button
+        loading={isLoading}
+        size="xs"
+        color="error"
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
         Unlink
       </Button>
       {error && <p className="text-sm text-error-content">{error}</p>}
--- app/components/navbar/userButton.tsx
+++ app/components/navbar/userButton.tsx
@@ -4,6 +4,11 @@ import { user as userSchema } from "@/db/schemas/auth-schema";
 import UserAvatar from "../userAvatar";
 import {
   Button,
+  Card,
+  CardActions,
+  CardText,
+  CardTitle,
+  Dialog,
   Divider,
   Dropdown,
   DropdownContent,
@@ -26,6 +31,7 @@ export default function UserButton({
 }: {
   user: typeof userSchema.$inferSelect;
 }) {
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
@@ -54,66 +60,94 @@ export default function UserButton({
   }
 
   return (
-    <Dropdown
-      requireClick
-      strategy="fixed"
-      isOpen={isOpen}
-      onOpenChange={setIsOpen}
-      placement="bottom-end"
-    >
-      <DropdownTrigger>
-        <Button
-          variant="ghost"
-          color="main"
-          onClick={() => {
-            navbar?.setIsOpen(false);
-          }}
-          className="flex p-2 items-center cursor-pointer gap-2 h-10"
-        >
-          <UserAvatar
-            avatarUrl={user.image || placeholderImage.src}
-            name={user.name || ""}
-          />
-          <PiCaretDownBold
-            className={
-              !isOpen
-                ? "rotate- transition-transform"
-                : " rotate-180 transition-transform"
-            }
-          />
-        </Button>
-      </DropdownTrigger>
-      <DropdownContent>
-        <DropdownTitle className="sm:hidden">{user.name}</DropdownTitle>
-        <Divider className="opacity-10 my-2 sm:hidden" />
-        <DropdownList>
-          <DropdownListItem as={Link} href={`/user/${user.username}`}>
-            <span>
-              <PiUser className="inline mr-4 sm:mr-2" />
-              Profile
-            </span>
-          </DropdownListItem>
-          <DropdownListItem as={Link} href={`/settings`}>
-            <span>
-              <PiGear className="inline mr-4 sm:mr-2" />
-              Settings
-            </span>
-          </DropdownListItem>
-          <Divider className="opacity-10 my-2" />
-          <DropdownListItem color="error" onClick={signout}>
-            {error ? (
-              error
-            ) : isLoading ? (
-              "working..."
-            ) : (
+    <>
+      <Dialog isOpen={isConfirmationOpen} setIsOpen={setIsConfirmationOpen}>
+        <Card className="w-[90vw] max-w-60">
+          <form action={signout}>
+            <CardTitle className="font-heading">{SIGNOUT}</CardTitle>
+            <CardText>Are you sure?</CardText>
+            <CardActions className="flex justify-between w-full">
+              <Button
+                color="success"
+                size="sm"
+                type="button"
+                onClick={() => setIsConfirmationOpen(false)}
+              >
+                NO
+              </Button>
+              <Button size="sm" color="error">
+                YES
+              </Button>
+            </CardActions>
+          </form>
+        </Card>
+      </Dialog>
+      <Dropdown
+        requireClick
+        strategy="fixed"
+        isOpen={isOpen}
+        onOpenChange={setIsOpen}
+        placement="bottom-end"
+      >
+        <DropdownTrigger>
+          <Button
+            variant="ghost"
+            color="main"
+            onClick={() => {
+              navbar?.setIsOpen(false);
+            }}
+            className="flex p-2 items-center cursor-pointer gap-2 h-10"
+          >
+            <UserAvatar
+              avatarUrl={user.image || placeholderImage.src}
+              name={user.name || ""}
+            />
+            <PiCaretDownBold
+              className={
+                !isOpen
+                  ? "rotate- transition-transform"
+                  : " rotate-180 transition-transform"
+              }
+            />
+          </Button>
+        </DropdownTrigger>
+        <DropdownContent>
+          <DropdownTitle className="sm:hidden">{user.name}</DropdownTitle>
+          <Divider className="opacity-10 my-2 sm:hidden" />
+          <DropdownList>
+            <DropdownListItem as={Link} href={`/user/${user.username}`}>
               <span>
-                <PiSignOut className="inline mr-4 sm:mr-2" />
-                {SIGNOUT}
+                <PiUser className="inline mr-4 sm:mr-2" />
+                Profile
               </span>
-            )}
-          </DropdownListItem>
-        </DropdownList>
-      </DropdownContent>
-    </Dropdown>
+            </DropdownListItem>
+            <DropdownListItem as={Link} href={`/settings`}>
+              <span>
+                <PiGear className="inline mr-4 sm:mr-2" />
+                Settings
+              </span>
+            </DropdownListItem>
+            <Divider className="opacity-10 my-2" />
+            <DropdownListItem
+              color="error"
+              onClick={() => {
+                setIsConfirmationOpen(true);
+              }}
+            >
+              {error ? (
+                error
+              ) : isLoading ? (
+                "working..."
+              ) : (
+                <span>
+                  <PiSignOut className="inline mr-4 sm:mr-2" />
+                  {SIGNOUT}
+                </span>
+              )}
+            </DropdownListItem>
+          </DropdownList>
+        </DropdownContent>
+      </Dropdown>
+    </>
   );
 }
